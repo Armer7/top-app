fb8d224157864185b835422876e5f701c9cf2bc4
Aleksey Poslushnyak <Poslushnyak@2amigos.us>
2021-06-15 13:47:10 +0300

    Add config for use svg files in js|ts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   // Webpack 5 is enabled by default
   // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
-  webpack5: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
