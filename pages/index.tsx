cab61d4edc17f3ddca47bfb4ad6ed2997754644a
Aleksey Poslushnyak <Poslushnyak@2amigos.us>
2021-06-17 15:51:00 +0300

    Set up ssr module in pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from 'next';
 import { Button, Htag, P, Rating, Tag } from '../components';
 import React, { useState } from 'react';
 import { withLayout } from '../Layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
   return (
     <>
@@ -23,8 +26,34 @@ function Home(): JSX.Element {
       <Tag color={'green'}>Green</Tag>
       <Tag color={'primary'}>Primary</Tag>
       <Rating rating={rating} isEditable={true} setRating={setRating} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
 }
 
 export default withLayout(Home);
